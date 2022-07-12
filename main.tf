5232c8b373f036a92eb79e26089eab5ca42b5749
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-07-12 09:05:09 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,24 @@
+
+variable "access_key" {}
+variable "secret_key" {}
+
+variable "region" {
+ default = "us-east-1"
+}
+
+
+provider "aws" {
+ access_key = "${   var.access_key }"
+ secret_key = var.secret_key
+ region     = "${  var.region}"
+ version    = ">= 3.0"
+}
+
+ resource "aws_ebs_volume" "awsEbsExample" {
+  availability_zone = "us-east-1a"
+  size              = var.sampleObject["size"]
+
+  tags = {
+    Name = var.sampleObject["tag1"]
+  }
+}
