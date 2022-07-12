73e099c1a14d241a0066945509378852dfcdd2cd
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-07-12 09:12:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,6 @@
 
-variable "sampleObject" {
-  type = object({size=number,tag1=string,tag2=string, isDev=bool})
-  default = {
-    size: 40
-    tag1= "Iac Object Volume Tag", 
-    tag2 = "Third Volume Tag"
-    isDev = true
-  }
+variable "ebsTag" {
+}
+
+variable "sizeEBS" {
 }
